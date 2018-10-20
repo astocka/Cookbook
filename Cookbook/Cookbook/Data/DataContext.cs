98eacfacd3d671e9b021ad05eefab3ddfb12867b
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2018-10-21 00:43:31 +0200

    app settings, add API Controller, DataContext and RecipeModel

---
+++ Cookbook/Cookbook/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cookbook.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cookbook.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public  DbSet<Recipe> Recipes { get; set; }
+    }
+}
