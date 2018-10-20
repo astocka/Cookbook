4f32be72b3843173ff3b5f2a0c2d924026eb72e8
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2018-10-21 01:20:34 +0200

    update RecipeModel, add Angular client side app

--- Cookbook/Cookbook/Models/Recipe.cs
+++ Cookbook/Cookbook/Models/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace Cookbook.Models
     public class Recipe
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
         public decimal Time { get; set; }
         public int Portion { get; set; }
