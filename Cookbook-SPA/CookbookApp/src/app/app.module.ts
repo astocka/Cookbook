69ac045971a495ec2adf495658c96c70aa9b009f
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2018-10-21 02:03:02 +0200

    add bootswatch, fontawesome and home, nav, recipe-list components

--- Cookbook-SPA/CookbookApp/src/app/app.module.ts
+++ Cookbook-SPA/CookbookApp/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
